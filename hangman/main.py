b9bcca7673319434aec269c81e9160772734f6eb
turevycvlad <v.o.turevych@student.khai.edu>
2024-10-22 12:38:10 +0300

    second stage

--- hangman/main.py
+++ hangman/main.py
@@ -1,2 +1,9 @@
 print("HANGMAN")
-print("The game will be available soon.")
+
+secret_word = 'python'
+guess = input("Guess the word: > ")
+
+if guess.lower() == secret_word:
+    print("You survived!")
+else:
+    print("You lost!")
