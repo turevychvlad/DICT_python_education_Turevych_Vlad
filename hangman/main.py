c4f745ba7d39403e06e699f8e69ae80747c13ef7
turevycvlad <v.o.turevych@student.khai.edu>
2024-10-22 13:00:33 +0300

    fourth stage

--- hangman/main.py
+++ hangman/main.py
@@ -5,7 +5,10 @@ print("HANGMAN")
 words = ['python', 'java', 'javascript', 'php']
 secret_word = random.choice(words)
 
-guess = input("Guess the word: > ")
+hint = secret_word[:3] + '-' * (len(secret_word) - 3)
+print(f"Guess the word: {hint}")
+
+guess = input("> ")
 
 if guess.lower() == secret_word:
     print("You survived!")
