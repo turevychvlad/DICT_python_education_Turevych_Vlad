00c63f359b5ef93a75c1f5d2a64f1cae973f7f29
turevycvlad <v.o.turevych@student.khai.edu>
2024-10-22 13:25:32 +0300

    second stage

--- dinnerparty/main.py
+++ dinnerparty/main.py
@@ -8,4 +8,10 @@ else:
         name = input("Enter the name of a friend (including you): ")
         friends[name] = 0
 
-    print(friends)
+    total_amount = float(input("Enter the total amount: "))
+    split_amount = round(total_amount / num_friends, 2)
+
+    for name in friends:
+        friends[name] = split_amount
+
+    print(friends) 
